1a86b08291c2a37b4a7b4aa9febf4c828b1197b8
OlgaDery <your.naviguide@gmail.com>
2021-12-12 11:46:19 -0700

    started project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProHappyMultiplatform"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/prohappymultiplatform/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.prohappymultiplatform
+
+expect class Platform() {
+    val platform: String
+}
