8a492fa08204bd5d2ba7f78c23379ef03be528b9
OlgaDery <your.naviguide@gmail.com>
2021-12-13 23:36:39 -0700

    started adding kotlin db classes for the shared module

---
+++ shared/src/androidMain/kotlin/cache/DatabaseDriverFactory.kt
@@ -0,0 +1,12 @@
+package cache
+
+import android.content.Context
+import com.example.testapp.shared.cache.AppDatabase
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+
+actual class DatabaseDriverFactory(private val context: Context) {
+    actual fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(AppDatabase.Schema, context, "test.db")
+    }
+}
---
+++ shared/src/iosMain/kotlin/cache/DatabaseDriverFactory.kt
@@ -0,0 +1,11 @@
+package cache
+
+import com.example.testapp.shared.cache.AppDatabase
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+
+actual class DatabaseDriverFactory {
+    actual fun createDriver(): SqlDriver {
+        return NativeSqliteDriver(AppDatabase.Schema, "test.db")
+    }
+}
